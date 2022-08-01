7ff132449c9d181360df182a2c87a0eced3ad987
Aaron Lambley <aaron.lambley@gmail.com>
2022-08-01 10:30:32 +0100

    Add more console feedback - "Seeding Object". Change randomisation of costume name and desc. Change booking start and end time to be random.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
 
-# seed for users
-
+puts "Seeding Users"
 12.times do
   User.create!(
     first_name: Faker::Name.neutral_first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
     password: '123456'
-    # is_owner: [true, false].sample
+
   )
 end
 
 puts "You have created #{User.count} users.."
 
-# seed for costumes
-
+puts "Seeding Costumes"
 36.times do
   Costume.create!(
-    name: Faker::Lorem.paragraph,
-    description: "lorem ipsum",
+    name: Faker::Company.profession,
+    description: Faker::Lorem.paragraph,
     animal_type: %w[dog cat].sample,
     user_id: 1
   )
@@ -34,14 +25,15 @@ end
 
 puts "Generated #{Costume.count} costumes"
 
-# seed for bookings
+puts "Seeing Bookings"
 
 12.times do
+  date_start = Faker::Time.forward(days: rand(1..30))
   Booking.create!(
     user_id: User.ids.sample,
     costume_id: Costume.ids.sample,
-    date_start: Faker::Time.backward(days: 14, period: :evening),
-    date_end: Faker::Time.forward(days: 23, period: :morning)
+    date_start:,
+    date_end: date_start + rand(1..30).day
   )
 end
 
