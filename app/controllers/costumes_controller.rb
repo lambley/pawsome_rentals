a7adfafbbd66e922842474410d1da60a420e88ff
Norbert Csipak <norbertcipak@me.com>
2022-08-01 12:08:12 +0100

    made index page in costumes controller - it lists out all the costumes on the page

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,4 +1,5 @@
 class CostumesController < ApplicationController
   def index
+    @costumes = Costume.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "costumes#index"
+  # get 'costumes', to: 'costumes#index', as: :list_costumes
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
