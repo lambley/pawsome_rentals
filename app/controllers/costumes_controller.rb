0d295d1cdce64200ceb83d83fd5c82e44531698e
laura reimarte <reimarte.laura@gmail.com>
2022-08-02 20:16:28 +0100

    add index and show views

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -2,4 +2,8 @@ class CostumesController < ApplicationController
   def index
     @costumes = Costume.all
   end
+
+  def show
+    @costume = Costume.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
   devise_for :users
-  root to: "costumes#index"
-  # get 'costumes', to: 'costumes#index', as: :list_costumes
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :costumes, only: %i[index show]
 end
