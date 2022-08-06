53c825851df1d01398aab69cfa6a24aeea6822b6
laura reimarte <reimarte.laura@gmail.com>
2022-08-06 12:23:25 +0100

    create new controller route view

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -7,9 +7,19 @@ class CostumesController < ApplicationController
     @costume = Costume.find(costume_params)
   end
 
+  def new
+    @costume = Costume.new
+  end
+
+  def create
+    @costume = Costume.new(costume_params)
+    @costume.save
+    redirect_to costume_path(@costume)
+  end
+
   private
 
   def costume_params
-    params.require(:costume).permit(:name, :description, :photo)
+    params.require(:costume).permit(:name, :description, :photo, :animal_type, :latitude, :longitude, :user_id)
   end
 end
