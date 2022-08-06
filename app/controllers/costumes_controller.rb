216f58dd5e111c2b93b5c6430092fd24ce226aef
Aaron Lambley <aaron.lambley@gmail.com>
2022-08-06 10:29:53 +0100

    add long lat marker array to index

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,6 +1,12 @@
 class CostumesController < ApplicationController
   def index
     @costumes = Costume.all
+    @markers = @costumes.map do |c|
+      {
+        lat: c.latitude,
+        long: c.longitude
+      }
+    end
   end
 
   def show
