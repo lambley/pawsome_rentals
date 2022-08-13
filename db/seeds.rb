fd8f4a0152b7213107c157eb28b1f03d049c6854
Norbert Csipak <norbertcipak@me.com>
2022-08-13 15:30:09 +0100

    images added to index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,18 @@ end
 puts "> Generated #{User.count} users"
 
 puts "Seeding Costumes"
+photos = ['https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399641/download_m6vfgc.jpg',
+          'https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399641/images_gm8vb4.jpg',
+          'https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399641/1658782280-3344685-center-3_kdm7o2.jpg',
+          'https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399641/1656456160-halloween-2-michael-myers-dog-costume_mqwqls.jpg',
+          'https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399640/02_Cat_Halloween_Costumes-Beetlejuice_Inline_jq0mlq.jpg',
+          'https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399641/image_vx11lq.webp',
+          'https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399640/2_wbsi7l.jpg',
+          'https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399641/madonna-dog-costume_tng8r5.jpg',
+          'https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399641/cat-halloween-costume-mermaid_zfaeyl.jpg',
+          'https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399641/images-1_ezgchx.webp',
+          'https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399641/1657567974-580413_PS_PAW_BILL_SUIT-scaled_jgpr3u.jpg',
+          'https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660399641/81PCVE1OhHL._AC_SL1500__ham1kr.jpg']
 
 36.times do
   longitude = rand(51.500..51.599).round(3)
@@ -35,7 +47,7 @@ puts "Seeding Costumes"
     city: address.first.city,
     user_id: 1
   )
-  c.photo.attach(io: URI.open('https://res.cloudinary.com/le-wagon-london-campus-batch-904/image/upload/v1660254926/karsten-winegeart-ewfHXBcuFA0-unsplash_cffclz.jpg'), filename: 'image.jpg')
+  c.photo.attach(io: URI.open(photos.sample), filename: 'image.jpg')
 end
 
 puts "> Generated #{Costume.count} costumes"
